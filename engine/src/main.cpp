6aaaf94925bb29a66dbbbbfbdcc68ff4e792468d
Nongtajkrub <sunanborthwic@gmail.com>
2026-02-28 14:08:16 +0700

    Testing new AST output function.

--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -19,9 +19,9 @@ int main() {
 	/*
 	for (const auto token : lexer.get_token()) {
 		if (token.lexeme.has_value()) {
-			std::cout << token.type_as_str() << ", " << token.lexeme.value() << '\n'; 
+			std::cout << token.kind_as_str() << ", " << token.lexeme.value() << '\n'; 
 		} else {
-			std::cout << token.type_as_str() << '\n'; 
+			std::cout << token.kind_as_str() << '\n'; 
 		}
 	}
 	*/
@@ -30,43 +30,11 @@ int main() {
 
 	auto parser = scr::Parser(lexer.get_token(), arena);
 	if (!parser.parse()) {
+		std::cout << "Prase Fail" << '\n';
 		return -1;
 	}
 
-	auto ast = parser.get_ast(1);
-
-	std::cout << "Node kind: " << static_cast<int>(*ast.adr) << '\n';
-
-	auto statment = reinterpret_cast<const scr::DeclarationStmt*>(ast.adr);
-
-	std::cout << "Identifier: " << statment->name.lexeme.value() << '\n';
-
-	auto expression =
-		reinterpret_cast<const scr::BinaryExpr*>(statment->init.value().adr);
-
-	auto left =
-		reinterpret_cast<const scr::PrimaryExpr*>(expression->left.adr);
-
-	std::cout << "left: " << left->token.lexeme.value() << '\n';
-
-	std::cout << "First level op: " << expression->op.kind_as_str() << '\n';
-
-	std::cout << "Expression Kind: " << static_cast<int>(*expression->right.adr) << '\n';
-
-	auto right =
-		reinterpret_cast<const scr::BinaryExpr*>(expression->right.adr);
-
-	auto inner_left = reinterpret_cast<const scr::PrimaryExpr*>(right->left.adr);
-	
-	std::cout << "right: left: " << inner_left->token.lexeme.value() << '\n';
-
-	std::cout << "Second level op: " << right->op.kind_as_str() << '\n';
-
-	auto inner_right =
-		reinterpret_cast<const scr::PrimaryExpr*>(right->right.adr);
-
-	std::cout << "right: right: " << inner_right->token.lexeme.value() << '\n';
-
+	scr::ast_output(std::cout, parser.get_ast(0));
 	/*
 	auto inner_expression = 
 		reinterpret_cast<const scr::BinaryExpr*>(expression->right.adr);
